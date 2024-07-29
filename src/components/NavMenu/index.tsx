f3d96a03897f9d619714ddd2d426435f8c1f54c9
albuquerque-lucas <lucaslpra@gmail.com>
2024-07-29 14:22:16 -0300

    Instalando tailwind.css e iniciando componentes.

--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -7,12 +7,6 @@ const NavMenu = () => {
         <li>
           <a href="/home">Home</a>
         </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
       </ul>
     </nav>
   )
