b4e39430b28e2e08c8503084d6f156a005b92446
albuquerque-lucas <lucaslpra@gmail.com>
2024-07-29 16:15:17 -0300

    Trocando TailwindCss por Bootstrap para facilitar a responsividade

--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -3,29 +3,52 @@ import { NavLink } from "react-router-dom";
 
 const NavMenu = () => {
   return (
-    <nav className="mx-60">
-      <ul className="text-white flex space-x-4 items-center">
-        <li>
-          <NavLink to="/"> 
-            Home 
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/fretes" className="text-white hover:text-gray-300">
-            Fretes
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/filiais" className="text-white hover:text-gray-300">
-            Filiais
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/clientes" className="text-white hover:text-gray-300">
-            Clientes
-          </NavLink>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <NavLink className="navbar-brand" to="/">
+          Testello
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link text-white">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/home" className="nav-link text-white">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/fretes" className="nav-link text-white">
+                Fretes
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/filiais" className="nav-link text-white">
+                Filiais
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/clientes" className="nav-link text-white">
+                Clientes
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -4,15 +4,15 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="bg-gray-800 p-4">
+    <div className="d-flex flex-column min-vh-100">
+      <header className="bg-dark text-white py-3">
         <NavMenu />
       </header>
-      <main className="flex-grow">
+      <main className="flex-grow-1">
         <Outlet />
       </main>
-      <footer className="bg-gray-800 text-white p-4 text-center">
-        Testello - 2024
+      <footer className="bg-dark text-white py-3 text-center">
+        © 2024 Testello. Todos os direitos reservados.
       </footer>
     </div>
   );
