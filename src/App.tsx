2c4e1a2617d4b39a3bb3de6f8b3adcdaa56ebb98
albuquerque-lucas <lucaslpra@gmail.com>
2024-07-29 14:17:45 -0300

    Apagando estilos de App.css e index.css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { router } from './router';
+import { RouterProvider } from 'react-router-dom';
+import NavMenu from './components/NavMenu';
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <header>
+        <NavMenu />
+      </header>
+      <main>
+        <RouterProvider router={router} />
+      </main>
+      <footer>
+        
+      </footer>
     </>
   )
 }
---
+++ src/router/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import Home from '../pages/home';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  }
+]);
