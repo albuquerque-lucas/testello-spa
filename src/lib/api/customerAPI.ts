9c5d049c647980f6d32adb5203ce43f4bfd56b36
albuquerque-lucas <lucaslpra@gmail.com>
2024-07-30 10:10:23 -0300

    Implementando metodo de requisicao get Customers

---
+++ src/lib/api/customerAPI.ts
@@ -0,0 +1,12 @@
+import { axios } from "../axios";
+import { Customer } from "../types/customers";
+import { APIPaginatedResponse } from "../types/responses";
+
+export const getCustomers = async () => {
+  try {
+    const response = await axios.get<APIPaginatedResponse<Customer>>('/api/customers');
+    return response.data;
+  } catch (error: any) {
+    console.log('Ocorreu um erro tentando cuscar os clientes', error);
+  }
+}
