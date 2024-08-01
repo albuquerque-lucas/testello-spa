2c4eaefb2027d762828d93c33f05a16affc7d96d
albuquerque-lucas <lucaslpra@gmail.com>
2024-08-01 19:19:40 -0300

    Alterando configuracoes do axios para producao

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -1,25 +1,7 @@
 import Axios from 'axios';
 
-// export const axios = Axios.create({
-//   baseURL: 'https://www.albuquerqueincode.com',
-//   timeout: 10000,
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Accept': 'application/json',
-//   },
-// });
-
-// export const axiosFormData = Axios.create({
-//   baseURL: 'https://www.albuquerqueincode.com',
-//   timeout: 10000,
-//   headers: {
-//     'Content-Type': 'multipart/form-data',
-//     'Accept': 'application/json',
-//   },
-// });
-
 export const axios = Axios.create({
-  baseURL: 'http://localhost',
+  baseURL: 'https://www.albuquerqueincode.com',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
@@ -28,10 +10,28 @@ export const axios = Axios.create({
 });
 
 export const axiosFormData = Axios.create({
-  baseURL: 'http://localhost',
+  baseURL: 'https://www.albuquerqueincode.com',
   timeout: 10000,
   headers: {
     'Content-Type': 'multipart/form-data',
     'Accept': 'application/json',
   },
 });
+
+// export const axios = Axios.create({
+//   baseURL: 'http://localhost',
+//   timeout: 10000,
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Accept': 'application/json',
+//   },
+// });
+
+// export const axiosFormData = Axios.create({
+//   baseURL: 'http://localhost',
+//   timeout: 10000,
+//   headers: {
+//     'Content-Type': 'multipart/form-data',
+//     'Accept': 'application/json',
+//   },
+// });
