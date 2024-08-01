1f8af4740301d131f05190d295664a3775bb8a20
albuquerque-lucas <lucaslpra@gmail.com>
2024-08-01 12:00:15 -0300

    Alterando disposicao dos elementos na pagina inicial

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,27 +5,27 @@ const Home = () => {
     <div className="d-flex flex-column align-items-center pt-5">
       <h1 className="display-4 mb-4">Bem-vindo à Testello</h1>
       <p className="h5 mb-4 text-center">Esta é a página inicial. Escolha, dentre as opções, o aspecto que deseja administrar:</p>
-      <ul className="d-flex flex-column flex-md-row justify-content-center list-unstyled">
-        <li className="mb-3 mb-md-0 mx-md-2">
+      <ul className="d-flex flex-column justify-content-center align-items-center list-unstyled w-100 mt-5">
+        <li className="mb-3 w-100 d-flex justify-content-center">
           <NavLink
             to="/fretes"
-            className="btn btn-secondary w-100"
+            className="btn btn-secondary w-25"
           >
             Fretes
           </NavLink>
         </li>
-        <li className="mb-3 mb-md-0 mx-md-2">
+        <li className="mb-3 w-100 d-flex justify-content-center">
           <NavLink
             to="/filiais"
-            className="btn btn-secondary w-100"
+            className="btn btn-secondary w-25"
           >
             Filiais
           </NavLink>
         </li>
-        <li className="mx-md-2">
+        <li className="w-100 d-flex justify-content-center">
           <NavLink
             to="/clientes"
-            className="btn btn-secondary w-100"
+            className="btn btn-secondary w-25"
           >
             Clientes
           </NavLink>
